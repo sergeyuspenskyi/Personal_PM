d9953eb1fa880ef3ff94d8e9c4f758d246fd0348
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-25 12:30:36 +0200

    Fix to scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,5 @@
 from openpyxl import load_workbook
 wb = load_workbook('example.xlsx')
-sheet = wb.active
-for cellObj in sheet['A1':'E11']:
-    for cell in cellObj:
-        result = {cell.coordinate: cell.value}
-        print(result)
+worksheet = wb.active
+sheet_list = [{cell.coordinate: cell.value for cell in row_cells} for row_cells in worksheet.iter_rows()]
+print(sheet_list)
