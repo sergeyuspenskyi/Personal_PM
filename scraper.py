fb379f004f65f2f5c8890cb2fa9ba78ec590fccf
sergeyuspenskyi <sergey.uspenskyy@gmail.com>
2020-10-28 12:39:27 +0200

    Converted code into function.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
-from openpyxl import load_workbook
-wb = load_workbook('example.xlsx')
-worksheet = wb.active
-sheet_list = [{cell.coordinate: cell.value for cell in row_cells} for row_cells in worksheet.iter_rows()]
-print(sheet_list)
+def excel_file_scraper(excel_file) -> list:
+    """Returns list with dicts per row in scrapped excel file"""
+    from openpyxl import load_workbook
+    wb = load_workbook(excel_file)
+    worksheet = wb.active
+    sheet_list = [{cell.coordinate: cell.value for cell in row_cells} for row_cells in worksheet.iter_rows()]
+    return sheet_list
